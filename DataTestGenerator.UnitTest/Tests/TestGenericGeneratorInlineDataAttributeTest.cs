9ae3d0b83b8dc480eba4003279ba9ff694f7568b
Andres Gimenez Muñoz <andres.gimenez@outlook.com>
2019-12-30 20:19:18 +0100

    Delete test.

--- DataTestGenerator.UnitTest/Tests/TestGenericGeneratorInlineDataAttributeTest.cs
+++ DataTestGenerator.UnitTest/Tests/TestGenericGeneratorInlineDataAttributeTest.cs
@@ -38,19 +38,6 @@
             value.Should().BeLessOrEqualTo(10000);
         }
 
-        [Fact]
-        public void TestGenericGeneratorDoubleInterval()
-        {
-            for (double i = -23551.2213; i < 12553.32158; i += 1425.32)
-            {
-                for (double j = 12653.32158; j < 12533453.32158; j += 25783.35)
-                {
-                    var doubleGenerate = GenericGenerator.Instance.GenerateDouble(i, j);
-                    doubleGenerate.Should().BeInRange(i, j);
-                }
-            }
-        }
-
         [Theory]
         [InlineData(525)]
         [MemberData(nameof(IntMatrixData))]
